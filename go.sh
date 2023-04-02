db0b57cdd2bd7fb591b73ea932f666ccb88ae5a9
ryuk-jayant <jayantdubey2003@gmail.com>
2023-04-02 13:00:59 +0530

    first commit

---
+++ go.sh
@@ -0,0 +1,14 @@
+gofile=$1
+echo $gofile
+err="your address not found"
+
+location=$(ps -ef | awk '{
+		if($1 == v1)
+		       	print $3;}' v1=$gofile /home/JKJ-ryuk/test.txt)
+
+if ($location == "") then
+	echo $err
+else 
+	echo $location
+	cd $location
+fi
---
+++ save.sh
@@ -0,0 +1,4 @@
+tosave=$PWD
+name=$1
+echo "$1 :: $tosave" >> /home/JKJ-ryuk/test.txt
+echo "$tosave is saved with name $name"
